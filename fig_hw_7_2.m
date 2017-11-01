8bd12e973f444687188ab99c9ad948adab70d987
Seungbum Koo <doougsini@gmail.com>
2017-11-01 11:50:17 -0500

    change in function names

--- fig_hw_7_2.m
+++ fig_hw_7_2.m
@@ -19,6 +19,6 @@ print('hw7_2_2','-depsc')
 arrow_rot(0,0,3/4*pi,5/14*pi,.3,'ccw');  marker(0.1,.3,'1')
 arrow_rot(L/2,0,1/2*pi,5/14*pi,.3,'ccw');marker(L-.1,.3,'2')
 arrow_rot(L,0,1/4*pi,5/14*pi,.3,'ccw');  marker(L/2,.5,'3')
-arrow([L/2,-.5],[L/2,-.1],2);            marker(L/2,-.65,'4')
+arrow1([L/2,-.5],[L/2,-.1],2);           marker(L/2,-.65,'4')
 
 print('hw7_2_1','-depsc')
--- fig_midterm2_1.m
+++ fig_midterm2_1.m
@@ -6,7 +6,7 @@ hold on; axis image; axis off
 
 % It is good to maintain the width of the structure by 7
 % for structure-to-support configuration.
-L4 = 4;
+L4 = 3.5;
 
 fixed(0,0,3*pi/2)
 pinned(-.05,L4,3*pi/2)
@@ -17,9 +17,9 @@ beam([L4,0],[2*L4,0])
 truss([0,L4],[L4,.1])
 truss([0,L4],[2*L4,.1])
 
-label([0,-.45],[L4,-.45],'$L$')
-label([L4,-.45],[2*L4,-.45],'$L$')
-label([-.46,0],[-.46,L4],'$L$')
+arrow2([0,-.45],[L4,-.45],1);marker(L4/2,-.63,'$L$')
+arrow2([L4,-.45],[2*L4,-.45],1);marker(1.5*L4,-.63,'$L$')
+arrow2([-.5,0],[-.5,L4],1);marker(-.67,L4/2,'$L$')
 
 marker(-.3,-.3,'A')
 marker(L4,-.25,'B')
@@ -28,7 +28,7 @@ marker(0,L4+.2,'D')
 
 marker(L4/2,.2,'$EI$')
 marker(L4*3/2,.2,'$EI$')
-marker(L4/2-.5,L4/2,'$EA$')
+marker(L4/2-.3,L4/2,'$EA$')
 marker(L4*3/2-1.2,L4/2,'$EA$')
 
 print('midterm2_1','-deps')
