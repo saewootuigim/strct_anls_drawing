fc6ebd318e5890f72f077bdedb25c9a82aee9a1f
Seungbum Koo <doougsini@gmail.com>
2017-11-01 11:33:44 -0500

    add rotational arrows

---
+++ arrow_rot.m
@@ -0,0 +1,21 @@
+function arrow_rot(center_x,center_y,position,center_degree,r,dir)
+
+th = linspace(-center_degree/2,center_degree/2,20);
+P = zeros(length(th),2);
+for i = 1 : length(th)
+    P(i,1) = r*cos(th(i));
+    P(i,2) = r*sin(th(i));
+end
+A = [cos(position),sin(position);-sin(position),cos(position)];
+for i = 1 : length(th)
+    P(i,:) = P(i,:)*A;
+end
+plot(P(:,1)+center_x,P(:,2)+center_y,'k')
+
+if strcmp(dir,'ccw')
+    [X,Y] = arrowhead(center_x+P(end,1),center_y+P(end,2),position+center_degree/2.7);
+    fill(X,Y,'k')
+else
+    [X,Y] = arrowhead(center_x+P(1,1),center_y+P(1,2),position-center_degree/2.7+pi);
+    fill(X,Y,'k')
+end
